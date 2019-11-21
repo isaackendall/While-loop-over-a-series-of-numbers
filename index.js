f7f64e2a5fbb91932e7ed99a4c745ae2ef99ec4b
isaackendall <isaac@isaac-kendall.com>
2019-11-21 08:21:19 +0000

    solved

---
+++ index.js
@@ -0,0 +1,24 @@
+function loopASequence(start, end) {
+  // create a loop which runs if start is less than or equal to end
+  while (start <= end) {
+    console.log(start);
+    start++
+  }
+    // log current value of start to console
+    // increment value of start
+}
+
+loopASequence(2, 5);
+// console output:
+  // 2
+  // 3
+  // 4
+  // 5
+
+loopASequence(3, 7);
+// console output:
+  // 3
+  // 4
+  // 5
+  // 6
+  // 7
